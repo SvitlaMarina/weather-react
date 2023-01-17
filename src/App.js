65cd184e45a48767533215ba335ce9e5915426dc
Marina Khrystyianyn <svitlamarina@gmail.com>
2023-01-17 23:14:28 +0100

    added search

--- src/App.js
+++ src/App.js
@@ -1,15 +1,77 @@
-//import axios from "axios";
+import React, { useState } from "react";
+import axios from "axios";
 import "./css/styles.css";
 import CurentWeather from "./CurentWeather";
-import Header from "./Header";
 import Futter from "./Futter";
 import "bootstrap/dist/css/bootstrap.css";
+import CurentData from "./CurentData";
 export default function App() {
-  return (
-    <div className="App">
-      <Header />
-      <CurentWeather />
-      <Futter />
-    </div>
-  );
+  let [city, setCity] = useState("Talske");
+  let [weather, setWeather] = useState({ ready: false });
+
+  function setCurrentWeather(response) {
+    console.log(response.data);
+    setWeather({
+      city: response.data.city,
+      temperature: Math.round(response.data.temperature.current),
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+      img: response.data.condition.icon_url,
+      ready: true,
+    });
+  }
+  function search() {
+    axios.get(apiUrl).then(setCurrentWeather);
+  }
+
+  function handelSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+  function handelCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  const key = `37febb05294o365efadt844fbb50ef35`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}`;
+  if (weather.ready) {
+    return (
+      <div className="App">
+        <div className="header">
+          <CurentData data={weather.data} />
+          <div className="enter-city">
+            <form className="enter-city" onSubmit={handelSubmit}>
+              <div className="mb-1 mr-2 m-1">
+                <input
+                  type="text"
+                  className="form-control border-info"
+                  id="cityName"
+                  placeholder="Enter you city"
+                  onChange={handelCityChange}
+                />
+              </div>
+              <input
+                type="submit"
+                className="btn btn-outline-primary border-info mb-2 m-1"
+                id="submitCity"
+                value="Search"
+              />
+              {/* <button
+                type="button"
+                className="btn btn-outline-success border-info mb-2 m-1"
+                id="enterCurentCity">
+                Curent
+              </button> */}
+            </form>
+          </div>
+        </div>
+        <CurentWeather weather={weather} />
+        <Futter />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading";
+  }
 }
--- src/CurentWeather.js
+++ src/CurentWeather.js
@@ -1,51 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import CityTemperature from "./CityTemperature";
 import CurentImg from "./CurentImg";
 import DescriptionHumidityWind from "./DescriptionHumidityWind";
-import axios from "axios";
 import "./css/CurentWeather.css";
-export default function CurentWeather() {
-  let city = `Talske`;
-  let [weather, setWeather] = useState({
-    temperature: 12,
-    description: "clouds",
-    humidity: 34,
-    wind: 35,
-    img: "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-night.png",
-    ready: false,
-  });
-
-  function setCurrentWeather(response) {
-    console.log(response.data);
-    setWeather({
-      temperature: Math.round(response.data.temperature.current),
-      description: response.data.condition.description,
-      humidity: response.data.temperature.humidity,
-      wind: response.data.wind.speed,
-      img: response.data.condition.icon_url,
-      ready: true,
-    });
-  }
-
-  const key = `37febb05294o365efadt844fbb50ef35`;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}`;
-
-  if (weather.ready) {
-    return (
-      <section className="curent">
-        <CityTemperature city={city} temperature={weather.temperature} />
-        <div className="curent-img-weather">
-          <CurentImg icon={weather.img} />
-          <DescriptionHumidityWind
-            description={weather.description}
-            humidity={weather.humidity}
-            wind={weather.wind}
-          />
-        </div>
-      </section>
-    );
-  } else {
-    axios.get(apiUrl).then(setCurrentWeather);
-    return "Loading";
-  }
+export default function CurentWeather(props) {
+  return (
+    <section className="curent">
+      <CityTemperature
+        city={props.weather.city}
+        temperature={props.weather.temperature}
+      />
+      <div className="curent-img-weather">
+        <CurentImg icon={props.weather.img} />
+        <DescriptionHumidityWind
+          description={props.weather.description}
+          humidity={props.weather.humidity}
+          wind={props.weather.wind}
+        />
+      </div>
+    </section>
+  );
 }
